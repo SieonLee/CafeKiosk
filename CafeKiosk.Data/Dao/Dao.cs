fee494d54b84023b1cc781d7a96b0bb3263d4782
SieonLee <74527137+SieonLee@users.noreply.github.com>
2020-11-25 15:03:49 +0900

    초기 커밋.

---
+++ CafeKiosk.Data/Dao/Dao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeKiosk.Dao
+{
+    public class Dao
+    {
+       
+            public static HomeDao Home { get; } = new HomeDao();
+        public static CoffeeDao Coffee { get; } = new CoffeeDao();
+        public static DessertDao Dessert { get; } = new DessertDao();
+        public static JuiceDao Juice { get; } = new JuiceDao();
+        public static CartDao Cart { get; } = new CartDao();
+    }
+    
+}
+
---
+++ CafeKiosk/OrderForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CafeKiosk
+{
+    public partial class OrderForm : Form
+    {
+        public OrderForm()
+        {
+            InitializeComponent();
+            UCHome uCHome = new UCHome();
+            addUC(uCHome);
+        }
+        private void movesidepanel(Button btn)
+        {
+            panelslide.Top = btn.Top;
+            panelslide.Height = btn.Height;
+            
+        }
+
+        private void addUC(UserControl uc) //User Control 생성
+        {
+            panelCenter.Controls.Clear();
+            uc.Dock = DockStyle.Fill;
+            panelCenter.Controls.Clear();
+            panelCenter.Controls.Add(uc);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            movesidepanel(btnStart);
+
+        }
+        private void btnCoffee_Click(object sender, EventArgs e)
+        {
+            movesidepanel(btnCoffee);
+            UCCoffee uCCoffee = new UCCoffee();
+            addUC(uCCoffee);
+        }
+
+        private void btnJuice_Click(object sender, EventArgs e)
+        {
+            movesidepanel(btnJuice);
+            UCJuice uCJuice = new UCJuice();
+            addUC(uCJuice);
+        }
+
+        private void btnDessert_Click(object sender, EventArgs e)
+        {
+            movesidepanel(btnDessert);
+            UCDessert uCDessert = new UCDessert();
+            addUC(uCDessert);
+        }
+
+        private void btnCart_Click(object sender, EventArgs e)
+        {
+            movesidepanel(btnCart);
+            UCCartt uCCart = new UCCartt();
+            addUC(uCCart);
+        }
+    }
+}
