f4405e24f3795c1c680cbaafd2f2a7b032e69623
eotjr1226 <69032861+eotjr1226@users.noreply.github.com>
2020-11-26 10:27:41 +0900

    쥬수 이미지 업로드

--- CafeKiosk/NewFolder1/UCJuice.cs
+++ CafeKiosk/NewFolder1/UCJuice.cs
@@ -16,5 +16,15 @@ namespace CafeKiosk
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UCJuice_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
