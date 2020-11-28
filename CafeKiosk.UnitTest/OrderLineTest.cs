aaf852e376037e64e76412804fc29704f6740c30
dawnshine20@gmail.com <dawnshine20@gmail.com>
2020-11-28 16:47:11 +0900

    OrderLineTest Dao 함수추가 및 테스트 완료

--- CafeKiosk.UnitTest/OrderLineTest.cs
+++ CafeKiosk.UnitTest/OrderLineTest.cs
@@ -1,6 +1,7 @@
 ﻿using CafeKiosk.Data;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 
 namespace CafeKiosk.UnitTest
 {
@@ -14,6 +15,30 @@ namespace CafeKiosk.UnitTest
 
             Assert.IsTrue(count > 0);
         }
+
+        [TestMethod]
+        public void GetAllTest()
+        {
+            List<OrderLine> list = Dao.OrderLine.GetAll();
+
+            Assert.IsTrue(list.Count > 0);
+        }
+
+        [TestMethod]
+        public void GetByPKTest()
+        {
+            OrderLine orderline = Dao.OrderLine.GetByPK(1);
+
+            Assert.AreEqual(1, orderline.OrderLineID);
+        }
+        [TestMethod]
+        public void WrongGetByPKTest()
+        {
+            OrderLine orderline = Dao.OrderLine.GetByPK(1000);
+
+            Assert.IsNull(orderline);
+        }
+
         [TestMethod]
         public void InsertTest()
         {
@@ -29,17 +54,44 @@ namespace CafeKiosk.UnitTest
 
             Assert.AreEqual(oldCount + 1, newCount);
         }
-        
+
+        [TestMethod()]
+        public void DeleteByPKTest()
+        {
+            OrderLine entity = new OrderLine();
+            entity.OrderID = Dao.Order.GetMaxKey();
+            entity.MenuID = Dao.Menu.GetByPK(1).MenuID;
+
+            Dao.OrderLine.Insert(entity);
+
+            int orderLineId = Dao.OrderLine.GetMaxKey();
+            int oldCount = Dao.OrderLine.GetCount();
+
+            Dao.OrderLine.DeleteByPK(orderLineId);
+
+            int newCount = Dao.OrderLine.GetCount();
+
+            Assert.AreEqual(oldCount - 1, newCount);
+        }
+
         [TestMethod]
         public void GetByMenuIdTest()
         {
+            var orderLines = Dao.OrderLine.GetByMenuId(1);
 
+            Assert.IsTrue(orderLines.Count > 0);
+            Assert.IsTrue(orderLines.TrueForAll(x => x.MenuID == 1));
         }
 
         [TestMethod]
         public void GetByOrderId()
         {
+            var orderLines = Dao.OrderLine.GetByOrderId(1);
 
+            Assert.IsTrue(orderLines.Count > 0);
+            Assert.IsTrue(orderLines.TrueForAll(x => x.OrderID == 1));
         }
+
+        
     }
 }
